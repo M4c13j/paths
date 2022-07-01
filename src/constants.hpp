6a27bbdfcd08b01f62f893d0cb0fdaf511da5a73
M4c13j <maciekcx11@gmail.com>
2022-07-01 18:34:02 +0200

    struct Point eqiuvalent to vector2 but with ints

--- src/constants.hpp
+++ src/constants.hpp
@@ -14,4 +14,25 @@ const int N = 20;
 
 Color tileColors[] = {WHITE, BLACK, YELLOW, PINK, BLUE, GREEN, RED}; // available colors
 
+// struct that is a vector2 but with integers
+struct Point {
+    int x,y;
+
+    Point(){}; // empty constructor
+    Point( Vector2 &lhs ) {
+        x = (int)lhs.x;
+        y = (int)lhs.y;
+    }
+    
+    Point( int _x, int _y ) {
+        x = _x;
+        y = _y;
+    }
+
+    Vector2 ptov() {
+        return Vector2{(float)x, (float)y};
+    }
+
+};
+
 #endif
